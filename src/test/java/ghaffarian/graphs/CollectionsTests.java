6ef85401b5e4e84249085a86d7781a5572457f1f
Seyed Mohammad Ghaffarian <s.m.ghaffarian@gmail.com>
2019-01-19 14:40:16 +0330

    Cleanup

--- src/test/java/ghaffarian/graphs/CollectionsTests.java
+++ src/test/java/ghaffarian/graphs/CollectionsTests.java
@@ -2,14 +2,13 @@
 package ghaffarian.graphs;
 
 import ghaffarian.collections.IdentityLinkedHashSet;
-import java.util.LinkedHashSet;
 import java.util.Set;
 
 import static org.junit.Assert.*;
 import org.junit.*;
 
 /**
- * Testing graph operations.
+ * Testing collections.
  * 
  * @author Seyed Mohammad Ghaffarian
  */
@@ -17,40 +16,9 @@ public class CollectionsTests {
     
     @Test
     public void identitySetTest()  {
-
-        Set<String> idSet = new IdentityLinkedHashSet<>();
-        //java.util.Collections.newSetFromMap(new java.util.IdentityHashMap<>());
-        String str1 = new String("test-1");
-        String str2 = new String("test-2");
-        String str3 = new String("test-2");
-
-        assertTrue(idSet.add(str1));
-        assertFalse(idSet.add(str1));
-        assertTrue(idSet.contains(str1));
-        //
-        assertTrue(idSet.add(str2));
-        assertFalse(idSet.add(str2));
-        assertTrue(idSet.contains(str2));
-        assertFalse(idSet.contains(str3));
-        //
-        assertTrue(idSet.add(str3));
-        assertFalse(idSet.add(str3));
-        assertTrue(idSet.contains(str3));
-        assertEquals(3, idSet.size());
-        
         Atom hydrogen1 = new Atom("Hydrogen", "H");
         Atom hydrogen2 = new Atom("Hydrogen", "H");
         Atom oxygen = new Atom("Oxygen", "O");
-        
-        Set<Atom> normalSet = new LinkedHashSet<>();
-        
-        assertTrue(normalSet.add(oxygen));
-        assertTrue(normalSet.contains(oxygen));
-        assertTrue(normalSet.add(hydrogen1));
-        assertTrue(normalSet.contains(hydrogen1));
-        assertTrue(normalSet.contains(hydrogen2));
-        assertFalse(normalSet.add(hydrogen2));
-        assertEquals(2, normalSet.size());
         
         Set<Atom> identitySet = new IdentityLinkedHashSet<>(); 
         //java.util.Collections.newSetFromMap(new java.util.IdentityHashMap<>());
